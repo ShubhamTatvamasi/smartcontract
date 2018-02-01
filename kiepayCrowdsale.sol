bc29e487385ace03485186b07233e001e7662f39
Shubham Tatvamasi <shubhamtatvamasi@gmail.com>
2018-02-01 14:34:49 +0530

    update

--- kiepayCrowdsale.sol
+++ kiepayCrowdsale.sol
@@ -6,25 +6,29 @@ interface token {
 
 contract KEPCrowdsale {
 
-    address private beneficiary;
     uint256 public amountRaised;
+    address private beneficiary;
     address private contractAdmin;
 //  price of tokens per ether
-    uint256 public price = 1;
+    uint256 public price;
     token public tokenReward;
     mapping(address => uint256) public balanceOf;
    
     event FundTransfer(address backer, uint amount, bool isContribution);
 
 //  Constructor function 
-    function KEPCrowdsale(address ifSuccessfulSendTo, address contractAdministrator, address addressOfTokenUsedAsReward) public {
+    function KEPCrowdsale(address ifSuccessfulSendTo,
+    address contractAdministrator,
+    uint256 priceOfTokensPerEther,
+    address addressOfTokenUsedAsReward) public {
         beneficiary = ifSuccessfulSendTo;
         contractAdmin = contractAdministrator;
+        price = priceOfTokensPerEther;
         tokenReward = token(addressOfTokenUsedAsReward);
     }
 
 //  function without name is the default function that is called whenever anyone sends funds to a contract
-    function () payable public {
+    function () payable private {
         uint amount = msg.value;
         balanceOf[msg.sender] += amount;
         amountRaised += amount;
