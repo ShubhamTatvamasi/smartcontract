ab82bd5234ddbbae9db416f0e378256f9dd93bc3
Shubham Tatvamasi <shubhamtatvamasi@gmail.com>
2018-02-10 21:12:24 +0530

    added bonus function

--- kiepayCrowdsale.sol
+++ kiepayCrowdsale.sol
@@ -12,8 +12,7 @@ contract KEPCrowdsale {
 //  price of tokens per ether
     uint256 public price;
     token public tokenReward;
-    mapping(address => uint256) public balanceOf;
-   
+    
     event FundTransfer(address backer, uint amount, bool isContribution);
 
 //  Constructor function 
@@ -30,9 +29,37 @@ contract KEPCrowdsale {
 //  function without name is the default function that is called whenever anyone sends funds to a contract
     function () payable private {
         uint amount = msg.value;
-        balanceOf[msg.sender] += amount;
         amountRaised += amount;
-        tokenReward.transfer(msg.sender, amount * price);
+        uint tokenAmount = amount * price;
+        uint bonusAmount;
+
+        if (amount >= 100 ether) {
+            // 100 % extra tokens
+            bonusAmount = tokenAmount * 1;
+            tokenReward.transfer(msg.sender, tokenAmount + bonusAmount);
+        } else if (amount >= 90 ether) {
+            // 90 % extra tokens
+            bonusAmount = tokenAmount * 90 / 100;
+            tokenReward.transfer(msg.sender, tokenAmount + bonusAmount);
+        } else if (amount >= 80 ether) {
+            // 80 % extra tokens
+            bonusAmount = tokenAmount * 80 / 100;
+            tokenReward.transfer(msg.sender, tokenAmount + bonusAmount);
+        } else if (amount >= 70 ether) {
+            // 70 % extra tokens
+            bonusAmount = tokenAmount * 70 / 100;
+            tokenReward.transfer(msg.sender, tokenAmount + bonusAmount);
+        } else if (amount >= 60 ether) {
+            // 60 % extra tokens
+            bonusAmount = tokenAmount * 60 / 100;
+            tokenReward.transfer(msg.sender, tokenAmount + bonusAmount);
+        } else if (amount >= 50 ether) {
+            // 50 % extra tokens
+            bonusAmount = tokenAmount * 50 / 100;
+            tokenReward.transfer(msg.sender, tokenAmount + bonusAmount);
+        } else {
+            tokenReward.transfer(msg.sender, tokenAmount);   
+        }
         FundTransfer(msg.sender, amount, true);
         beneficiary.transfer(amount); // Transfer all the ethers amount to the beneficiary account
     }
