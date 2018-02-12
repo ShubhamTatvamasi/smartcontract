a15c39f38f098176f02034747b0334bc0b1a652c
Shubham Tatvamasi <shubhamtatvamasi@gmail.com>
2018-02-12 17:30:55 +0530

    update

--- kiepayToken.sol
+++ kiepayToken.sol
@@ -21,7 +21,7 @@ contract kiepayToken {
     /* Send coins */
     function transfer(address _to, uint256 _value) public {
         require(balanceOf[msg.sender] >= _value);           // Check if the sender has enough
-        require(balanceOf[_to] + _value >= balanceOf[_to]); // Check for overflows
+        require(balanceOf[_to] + _value > balanceOf[_to]);  // Check for overflows
         balanceOf[msg.sender] -= _value;                    // Subtract from the sender
         balanceOf[_to] += _value;                           // Add the same to the recipient
         Transfer(msg.sender, _to, _value);
